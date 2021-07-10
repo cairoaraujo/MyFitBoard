91416c8b0a40e7701817dbdd5c38430a7a2014ee
Cairo Araujo <cairobezerra@hotmail.com>
2021-07-10 17:56:25 -0300

    adding heartRate and distance charts and some fixes on dashboard

--- index.js
+++ index.js
@@ -6,22 +6,36 @@ function fazGet(url){
 }
 var dia = [];
 var calorias = [];
+var bpm = [];
+var distance = [];
 function main(){
     data = fazGet("http://localhost:3333/dados")
     meusDados = JSON.parse(data);
     console.log(meusDados)
 
     meusDados.map(function(meusDados,i){
-        calorias[i] = meusDados.value
+        calorias[i] = meusDados.kcal
         return calorias[i]
     })
 
     meusDados.map(function(meusDados,i){
-        dia[i] = meusDados.Data
+        dia[i] = meusDados.date
         return dia[i]
     })
+
+    meusDados.map(function(meusDados,i){
+        bpm[i] = meusDados.heartRate
+        return bpm[i]
+    })
+    meusDados.map(function(meusDados,i){
+        distance[i] = meusDados.distance
+        return distance[i]
+    })
+
     console.log(calorias)
     console.log(dia)
+    console.log(bpm)
+    console.log(distance)
 }
 main()
 
@@ -34,10 +48,70 @@ let chart = new Chart(primeiroGrafico,{
         datasets:[{
             label:"Gasto calórico",
             data: calorias,
-            backgroundColor:"#e35252"
+            backgroundColor:"#47E66F"
         }]
     },
     options:{
+
+        plugins:{
+            legend:{
+                labels:{
+                    font:{
+                        size:20
+                    }
+                }
+            },
+            title:{
+                display: true,
+                text:"IHUUU"
+            }
+        }
+    }
+});
+
+let segundoGrafico = document.getElementById('segundoGrafico').getContext('2d');
+let chart2 = new Chart(segundoGrafico,{
+    type: "line",
+    data:{
+        labels:dia,
+
+        datasets:[{
+            label:"Distância Percorrida",
+            data: distance,
+            backgroundColor:"#EB8700"
+        }]
+    },
+    options:{
+
+        plugins:{
+            legend:{
+                labels:{
+                    font:{
+                        size:20
+                    }
+                }
+            },
+            title:{
+                display: true,
+                text:"IHUUU"
+            }
+        }
+    }
+});
+
+let chart3 = new Chart(heartRate,{
+    type: "line",
+    data:{
+        labels:dia,
+
+        datasets:[{
+            label:"Heart rate",
+            data: bpm,
+            backgroundColor:"yellow"
+        }]
+    },
+    options:{
+
         plugins:{
             legend:{
                 labels:{
